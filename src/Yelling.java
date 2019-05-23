a6b0416a8d236713c793d5418db55c1877bd5dd1
sagarsaini3993 <39058650+sagarsaini3993@users.noreply.github.com>
2019-05-23 14:04:34 -0400

    R1-Red

--- src/Yelling.java
+++ src/Yelling.java
@@ -1,5 +1,8 @@
 
 public class Yelling {
 	
-	
+	public String Scream(String name) {
+		return "peter is yelling";
+	}
+
 }
--- test/YellingTest.java
+++ test/YellingTest.java
@@ -10,8 +10,11 @@ public class YellingTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOnePersonYelling() {
+		
+		Yelling y = new Yelling();
+		String actualOutput = y.Scream("Peter");
+		assertEquals("Peter is yelling", actualOutput);
 	}
 
 }
